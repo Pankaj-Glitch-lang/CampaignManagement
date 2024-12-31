31d64c681d9016179e3ae97cc754000f8af9a02a
Pankaj Kumar <pankajmahato0420@gmail.com>
2024-12-31 21:09:16 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const CampaignRouter = require('./router/campaign.router');
 const AdminRouter = require('./router/admin.router');
 dotenv.config()
 const app=express()
-const uri=mongodb_uri = "mongodb+srv://pankajmahato0089:eCwJm0kvbkdVMuql@cluster0.f21rb.mongodb.net/campaign-manager?retryWrites=true&w=majority&appName=Cluster0"
+const uri=process.env.mongodb_uri
+const PORT=process.env.PORT
 app.use(express.json())
 app.use(cors())
 const atlasDb= ()=>{
@@ -20,7 +21,7 @@ const atlasDb= ()=>{
 
 app.use('/api/campaigns',CampaignRouter)
 app.use('/admin',AdminRouter)
-app.listen(8080,(err)=>{
+app.listen(PORT,(err)=>{
     if(!err){
         console.log('Server Started..');
         atlasDb()
